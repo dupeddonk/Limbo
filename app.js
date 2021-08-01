c002037a0d04dc3acd67e182241933b56b321693
dupeddonk <88083951+dupeddonk@users.noreply.github.com>
2021-08-01 18:19:49 -0400

    update app.js
    refactored, added fs module import to store results in ./results.json

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 import crypto from 'crypto';
+import fs from 'fs';
+import { stdout } from 'process';
 
 //========================================================
+//set the server seed, must be in quotes
+let serverSeed = '<YOUR SERVER SEED>';  
 
-let serverSeed = '';  //enter server seed, must be in quotes
-let clientSeed = ''; // enter client seed, must be in quotes
-let firstNonce = 0; // set first nonce, 0 for new seed
-let finalNonce = 1000000; // set final nonce - the number of bets calculated will be (final nonce - first nonce)
+// set the client seed, must be in quotes
+let clientSeed = '<YOUR CLIENT SEED>';
 
+// set first nonce, 0 for new seed
+let firstNonce = 0; 
+
+// set final nonce - the number of bets calculated will be (final nonce - first nonce).  More bets = more time to execute.
+let finalNonce = 1000000; 
+
+//Add or remove any targets within the brackets, seperated by commas.  More targets = more time to execute.
+let targets = [1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
+
+
+// changing anything below this line might break something
 //========================================================
 
+let resultByTarget = [];
 
 const betCount = finalNonce - firstNonce;
 let nonces = Array.from({length: betCount}, (v, k) => k+1); 
+
+for (let i = 0; i < targets.length; i++) {
+    const target = targets[i];
+    
+}
+
+function winLossAmount(multiplier, numberOfHits){
+    return (numberOfHits * multiplier) - betCount
+  }
+ 
+  function hitRate(hitCount, sampleSize){
+      return ((hitCount / sampleSize) * 100) + '%'
+  }
+
+
+class Target {
+    constructor(target, hits, attempts){
+        this.target = target;
+        this.hits = hits;
+        this.attempts = attempts; 
+        this.expectedHitRate = parseFloat((((100/target) * 0.99) / 100).toFixed(7));
+        this.hitRate = parseFloat((hits / attempts).toFixed(7));
+        this.expectedHits = parseFloat((this.expectedHitRate * attempts).toFixed(7));
+        this.expectedHitsVsHits = parseFloat((hits - this.expectedHits).toFixed(7));
+        this.expectedHitRateVsHitRate = parseFloat((this.hitRate - this.expectedHitRate).toFixed(7));
+        this.winLossAmount = (target * hits) - attempts;
+    }
+}
+
 
 
 function getLimboResult(serverSeed, clientSeed, nonce){
@@ -38,216 +81,42 @@ function getLimboResult(serverSeed, clientSeed, nonce){
 
 function simulateBets(){
   let results = []
-  let one = 0;
-  let multi1point01 = 0;
-  let multi1point5 = 0;
-  let multi2 = 0;
-  let multi4 = 0;
-  let multi5 = 0;
-  let multi10 = 0;
-  let multi100 = 0;
-  let multi1000 = 0;
-  let multi10000 = 0;
-  let multi100000 = 0;
-  let multi500000 = 0;
-  let multi750000 = 0;
-  let multi900000 = 0;
-  let multi999999 = 0;
+    for (let i = 0; i < targets.length; i++) {
+        const target = targets[i];
+        let hits = 0;
 
-  for (let i = 0; i < nonces.length; i++) {
-
-    const nonce = nonces[i];
-    let currentResult = getLimboResult(serverSeed, clientSeed, nonce)
-
-    results.push(currentResult);
-    if (currentResult > 999999){
-      multi999999 += 1;
-      multi900000 += 1;
-      multi750000 += 1;
-      multi500000 += 1;
-      multi100000 += 1;
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    }
-    
-    else if (currentResult > 900000){
-      multi900000 += 1;
-      multi750000 += 1;
-      multi500000 += 1;
-      multi100000 += 1;
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
+        for (let i = 0; i < nonces.length; i++) {
+            const nonce = nonces[i];
+            let currentResult = getLimboResult(serverSeed, clientSeed, nonce);
+            results.push(currentResult);
+            if(currentResult >= target){
+                hits += 1;
+            };
+        };
+        let currentTarget = new Target(target, hits, betCount);
+        resultByTarget.push(currentTarget);
     }
 
-    else if (currentResult > 750000){
-      multi750000 += 1;
-      multi500000 += 1;
-      multi100000 += 1;
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    }
-    else if (currentResult > 500000){
-      multi500000 += 1;
-      multi100000 += 1;
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    }
-    
-    else if (currentResult > 100000){
-      multi100000 += 1;
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    }
-    
-    else if (currentResult > 10000){
-      multi10000 += 1;
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    }
-    
-    else if (currentResult > 1000){
-      multi1000 += 1;
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    } else if (currentResult > 100){
-      multi100 += 1;
-      multi10 += 1;
-      multi5 += 1;
-      multi4 += 1;
-      multi2 +=1;
-      multi1point5 += 1;
-      multi1point01 += 1;
-    } else if (currentResult > 10){
-        multi10 += 1;
-        multi5 += 1;
-        multi4 += 1;
-        multi2 +=1;
-        multi1point5 += 1;
-        multi1point01 += 1;
-    } else if (currentResult > 5){
-        multi5 += 1;
-        multi4 += 1;
-        multi2 += 1;
-        multi1point5 += 1
-        multi1point01 += 1;
-    } else if
-    
-    
-    (currentResult > 4){
-        multi4 += 1;
-        multi2 += 1;
-        multi1point5 += 1
-        multi1point01 += 1;
-    } else if (currentResult > 2){
-        multi2 +=1;
-        multi1point5 += 1
-        multi1point01 += 1;
-    }
-    
-    else if (currentResult > 1.5){
-      multi1point5 += 1
-      multi1point01 += 1;
-    } else if (currentResult > 1.01){
-      multi1point01 += 1;
-    } else {
-      one += 1;
-    }
-    
-    
-  }
-
- function winLossAmount(multiplier, numberOfHits){
-   return (numberOfHits * multiplier) - betCount
- }
-
- function hitRate(hitCount, sampleSize){
-     return ((hitCount / sampleSize) * 100) + '%'
- }
-
- const average = (results.reduce((a, b) => a + b, 0)) / results.length
- let toDollars = Intl.NumberFormat('en-US',{
-    style: "currency",
-    currency: "USD",
-});
-  const resultByMultiplier = [
-   
-    ['999,999x', multi999999, toDollars.format(winLossAmount(999999, multi999999)), hitRate(multi999999, betCount)], 
-    ['900,000x', multi900000, toDollars.format(winLossAmount(900000, multi900000)), hitRate(multi900000, betCount)], 
-    ['750,000x', multi750000, toDollars.format(winLossAmount(750000, multi750000)), hitRate(multi750000, betCount)], 
-    ['500,000x', multi500000, toDollars.format(winLossAmount(500000, multi500000)), hitRate(multi500000, betCount)], 
-    ['100,000x', multi100000, toDollars.format(winLossAmount(100000, multi100000)), hitRate(multi100000, betCount)], 
-    ['10,000x', multi10000, toDollars.format(winLossAmount(10000, multi10000)), hitRate(multi10000, betCount)],
-    ['1,000x', multi1000, toDollars.format(winLossAmount(1000, multi1000)), hitRate(multi1000, betCount)], 
-    ['100x', multi100, toDollars.format(winLossAmount(100, multi100)), hitRate(multi100, betCount)], 
-    ['10x', multi10, toDollars.format(winLossAmount(10, multi10)), hitRate(multi10, betCount)],
-    ['5x', multi5, toDollars.format(winLossAmount(5, multi5)), hitRate(multi5, betCount)],
-
-    ['4x', multi4, toDollars.format(winLossAmount(4, multi4)), hitRate(multi4, betCount)],
-    ['2x', multi2, toDollars.format(winLossAmount(2, multi2)), hitRate(multi2, betCount)],
-    ['1.5x', multi1point5, toDollars.format(winLossAmount(1.5, multi1point5)), hitRate(multi1point5, betCount)], 
-    ['1.01x', multi1point01, toDollars.format(winLossAmount(1.01, multi1point01)), hitRate(multi1point01, betCount)], 
-    ['1.0 (loss)', one, 'XXX']];
-    
-    console.log('Server Seed: ' + serverSeed);
-    console.log('Client Seed: ' + clientSeed);
-    console.log('Starting Nonce: ' + firstNonce);
-    console.log('Number of Bets: ' + betCount);
-    console.log('Amount of each bet: $1')
-    console.log('Theoretical Expected Loss: ' + toDollars.format((betCount * 0.01)));
-    console.log('| index | target multiplier | wins | win/loss amount |')
-  console.table(resultByMultiplier);
-
-  return results;
+    fs.writeFile("results/results.json", JSON.stringify(results), err => {
+        if (err) {
+            throw err;
+        }
+        console.log('Each rolled number has been saved to \'./results.json\' . Running this program again will ovewrite the file. ')
+    })
+  return resultByTarget;
 }
+
+let summary = [
+  {'Server Seed': serverSeed, 'Player Seed': clientSeed, 'Sample Size': betCount},
+]
 console.time('time to execute')
-simulateBets();
+console.table(summary);
+console.log('Simulating Limbo Bets...')
+console.table(simulateBets())
 console.timeEnd('time to execute')
+
+
+
 
 
 
